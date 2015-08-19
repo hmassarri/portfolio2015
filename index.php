e9c0b484c87ca2c82dbb8eec85b0ba73afc14ccd
hmassarri <hugo@hugomassarri.com>
2015-08-19 16:53:26 -0400

    cleaned up files and code for production

--- index.php
+++ index.php
@@ -1,59 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Hugo M. Freelance Developer / Designer</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/stylish-portfolio.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
+<?php require_once 'includes/structure.php'; ?>
+<?php printHeader(); ?>
 <body>
     
     <!-- Navigation -->
-    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
-    <nav id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
-            <li class="sidebar-brand">
-                Main Menu
-            </li>
-            <li>
-                <a href="#top" onclick = $("#menu-close").click(); >Home</a>
-            </li>
-            <li>
-                <a href="#about" onclick = $("#menu-close").click(); >About</a>
-            </li>
-            <li>
-                <a href="#projects" onclick = $("#menu-close").click(); >Projects</a>
-            </li>
-            <li>
-                <a href="#contact" onclick = $("#menu-close").click(); >Contact</a>
-            </li>
-        </ul>
-    </nav>
+    <?php navMenu(); ?>
 
     <!-- Header -->
     <header class="header" id="top">
@@ -72,18 +22,8 @@
 
 
     <!-- About -->
-    <section id="about" class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Visual Designer / Front End Developer<br />Design Junkie / Part-time Bike Builder</h2>
-                        <p class="lead"><i>Let's collaborate on your next project :)</i></p>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /.container -->
-    </section>
+    <?php printQuotes("Visual Designer / Front End Developer<br />Design Junkie / Part-time Bike Builder", "Let's collaborate on your next project :)", "about"); ?>
+
 
     <!-- Skills -->
     <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
@@ -93,256 +33,35 @@
                 <div class="col-lg-10 col-lg-offset-1">
                     <h2>Skills</h2>
                     <hr class="small">
+
                     <div class="row">
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-html5 fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>99%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-css3 fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>95%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-code fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>95%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-coffee fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>99%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-apple fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>99%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-joomla fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>98%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-drupal fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>89%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-wordpress fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>85%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-linux fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>90%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-codepen fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>81%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-bicycle fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>99%</strong></h4>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-6">
-                            <div class="service-item">
-                                <span class="fa-stack fa-4x">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-motorcycle fa-stack-1x text-primary"></i>
-                                </span>
-                                <h4><strong>99%</strong></h4>
-                            </div>
-                        </div>
+                        
+                        <?php printSkills(); ?>
+
                     </div><!-- /.row (nested) -->
                 </div><!-- /.col-lg-10 -->
             </div><!-- /.row -->
         </div><!-- /.container -->
     </section>
 
-    
     <!-- Quote -->
-    <section id="about" class="quote1">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Design is not just what it looks like and feels like. Design is how it works</h2>
-                        <p class="lead"><i>-Steve Jobs</i></p>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /.container -->
-    </section>
-
+    <?php printQuotes("Design is not just what it looks like and feels like. Design is how it works.", "-Steve Jobs", "quote1"); ?><!-- Page Content -->
 
     <div class="container" id="projects">
-        <!-- Project Row 0 -->
+        <!-- Project Row 1 -->
+
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="page-header">Projects</h2>
             </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#chad">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/chad.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#chem">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/chem.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#convocation">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/convocation.png">
-                </a>
-            </div>
-        </div><!-- /.row -->
-
-        <!-- Project Row 1 -->
-        <div class="row">
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#decadence">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/decadence.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#hpcc">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/hpcc.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#iamdn">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/iamdn.png">
-                </a>
-            </div>
-        </div><!-- /.row -->
-
-
-        <!-- Project Row 2 -->
-        <div class="row">
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#irw">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/irw.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#juma">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/juma.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#math">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/math.png">
-                </a>
-            </div>
-        </div><!-- /.row -->
-
-
-        <!-- Project Row 3 -->
-        <div class="row">
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#mountainview">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/mountainview.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#rei">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/rei.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#showmay">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/showmay.png">
-                </a>
-            </div>
-        </div><!-- /.row -->
-
-
-        <!-- Project Row 4 -->
-        <div class="row">
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#unidentified">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/unidentified.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#valentine">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/valentine.png">
-                </a>
-            </div>
-            <div class="col-sm-4 col-xs-12">
-                <a href="project-items.php#wram">
-                    <img alt="" class="img-responsive portfolio-item thumb thumb" src="img/web/wram.png">
-                </a>
-            </div>
+            <?php printProjects(); ?>
         </div><!-- /.row -->
         <hr>
     </div><!-- /.container -->
     
 
     <!-- Quote -->
-    <!-- Quote -->
-    <section id="about" class="quote4">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Good design is as little design as possible</h2>
-                        <p class="lead"><i>-Deiter Rams</i></p>
-                </div>
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /.container -->
-    </section>
+    <?php printQuotes("Good design is as little design as possible", "-Deiter Rams", "quote3"); ?>
 
     <!-- Call to Action
     <aside class="call-to-action bg-primary">
@@ -372,39 +91,8 @@
 
 
     <!-- Footer -->        
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 col-lg-offset-1 text-center">
-                    <h4><strong>Hugo M.</strong>
-                    </h4>
-                    <ul class="list-unstyled">
-                        <li><i class="fa fa-phone fa-fw"></i> (408) 805-4846</li>
-                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:hugo@hugomassarri.com">hugo@hugomassarri.com</a>
-                        </li>
-                    </ul>
-                    <br>
-                    <ul class="list-inline">
-                        <li>
-                            <a href="https://github.com/hmassarri" target="_blank"><i class="fa fa-github fa-fw fa-3x"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/blurtreynolds" target="_blank"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/hmassarri" target="_blank"><i class="fa fa-linkedin fa-fw fa-3x"></i></a>
-                        </li>
-                    </ul>
-                    <hr class="small">
-                    <p class="text-muted">Copyright &copy; Hugo M. / hugomassarri.com 2010-2015</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <div class="col-lg-12 text-center">
-        <a class="btn btn-lg btn-dark" href="#top">Back to Top</a>
-    </div>
-</body>
+    <?php printFooter(); ?>
+
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
@@ -443,4 +131,5 @@
             });
         });
     </script>
+</body>
 </html>
